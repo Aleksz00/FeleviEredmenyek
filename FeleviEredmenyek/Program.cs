302d6cb08cd68342464231cada57af8aa9fffce1
B19KosikA <B19KosikA@204-06>
2023-11-10 09:41:23 +0100

    Add project files.

---
+++ FeleviEredmenyek/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace FeleviEredmenyek
+{
+    class Program
+    {
+        static double atlag(List<felevi> a)
+        {
+
+            return a.Average(x => x.atlagSzam);
+        }
+
+        //static List<felevi> progi(List<felevi> b)
+        //{
+        //  var progi =b.Where(x => x.Programozas == 1 || x.ProgramozasGyak == 1).ToList();
+        //    return progi;
+
+        // }
+        static List<felevi> tan(List<felevi> b)
+        {
+            
+            var tantargyankent = new List<double>();
+            for (int i = 0; i < 8; i++)
+            {
+                tantargyankent.Add(0); 
+            }
+            for (int i = 0; i < b.Count; i++)
+            {
+                for (int a = 0; a < b[i].jegyek[a]; a++)
+                {
+                    tantargyankent[a] += b[i].jegyek[a];
+                }
+
+            }
+            for (int i = 0; i < tantargyankent.Count; i++)
+            {
+                tantargyankent[i] =tantargyankent[i]/8;
+
+               
+            }
+           
+        }
+
+        static void kiir(List<felevi> f)
+        {
+            using (var sw = new StreamWriter(@"..\..\src\adatok.txt"))
+            {
+                for (int i = 0; i < f.Count; i++)
+                {
+                    sw.WriteLine($"{f[i].TanuloNev}, {f[i].OktatasiAzonosito}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var felevik = new List<felevi>();
+            using (var sr = new StreamReader(@"..\..\..\src\felevi.txt.txt",Encoding.UTF8))
+            {
+                var tantargyak = sr.ReadLine().Split("\t").Skip(2).ToList();
+                while (!sr.EndOfStream)
+                {
+                    felevik.Add(new felevi(sr.ReadLine(),tantargyak));
+                    sr.ReadLine();
+                }
+                
+            }
+            Console.WriteLine(felevik.Count);
+
+
+            Console.WriteLine($"1.feladat: {atlag(felevik)}");
+            Console.WriteLine($"1.2.feladat");
+            Console.WriteLine($"1.3.feladat:");
+            Console.WriteLine("2.feladat");
+            var f2 = felevik.Where(x => x.jegyek[3] == 1).ToList();
+            Console.WriteLine(f2);
+            foreach (var item in felevik)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("3.feladat");
+            var f3=felevik.FirstOrDefault(t => t.jegyek[4] == 3);
+            Console.WriteLine(f3);
+            Console.WriteLine("4.feladat");
+            Console.Write("Kinek a legjobb jegyet szeretned?: ");
+            string nev = Console.ReadLine();
+            var kival = felevik.FirstOrDefault(t => t.TanuloNev == nev);
+            Console.WriteLine($"{kival.LegjobbJegy}");
+
+            Console.WriteLine("5.feladat");
+            kiir(felevik);
+
+
+        }
+    }
+}
---
+++ FeleviEredmenyek/felevi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FeleviEredmenyek
+{
+    class felevi
+    {
+        private List<string> tantargyak;
+        public string TanuloNev { get; set; }
+        public string OktatasiAzonosito { get; set; }
+        public List<int> jegyek;
+        public double Osztalyatlag { get; set; }
+
+        public double LegjobbJegy =>jegyek.Max();
+
+
+
+
+        public felevi(string r , List<string>tantargyak)
+        {
+            string[] s = r.Split("\t");
+            TanuloNev = s[0];
+            OktatasiAzonosito =s[1];
+            jegyek = new List<int>();
+            for (int i = 2; i < s.Length; i++)
+            {
+                jegyek.Add(int.Parse(s[i]));
+            }
+            this.tantargyak = tantargyak;
+        }
+
+        public double atlagSzam => jegyek.Average();
+       
+
+        
+        
+        public override string ToString()
+        {
+            string osztalyzat = $"Tanuló Neve: {TanuloNev}, Oktatási azonosito: {OktatasiAzonosito}, Jegyek:  ";
+            for (int i = 0; i < jegyek.Count; i++)
+            {
+                osztalyzat += "\n";
+                osztalyzat += tantargyak[i]+" "+ jegyek[i];  
+            
+            }
+            return osztalyzat ;
+        }
+    }
+}
